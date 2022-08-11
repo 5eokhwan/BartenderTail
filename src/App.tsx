841139639af8d914b62db2b696934723b1a4573c
exist909@naver.com <exist909@naver.com>
2022-08-11 16:02:46 +0900

    햄버거 아이콘

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
+import HambergerMenu from './components/HambergerMenu';
 import ModeToggle from './components/ModeToggle';
 import RecipeListPage from './pages/ReceipeListPage/ReceipeListPage';
 import GlobalStyle from './style/global';
@@ -10,6 +11,12 @@ const {light, dark} = mode;
 const ModeToggleWrapper = styled.div`
   position: fixed;
   padding: 5px 0 0 5px;
+`;
+
+const MenuBtnWrapper = styled.div`
+  position: fixed;
+  top: 2px;
+  right: 0;
 `;
 
 function App() {
@@ -24,6 +31,9 @@ function App() {
           <ModeToggleWrapper>
             <ModeToggle isLight={isLight} height={20} onToggle={toggleTheme}/>
           </ModeToggleWrapper>
+          <MenuBtnWrapper>
+            <HambergerMenu />
+          </MenuBtnWrapper>
           <RecipeListPage />
         </div>
       </ThemeProvider>
--- src/components/HambergerMenu.tsx
+++ src/components/HambergerMenu.tsx
@@ -1,5 +1,73 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const H = {
+    Wrapper: styled.div``,
+    ActiveBtn: styled.div<{isActive : boolean}>`
+        position: relative;
+        width: 25px;
+        height: 25px;
+        background: #37a6ff;
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: 0.2s;
+
+        &::before {
+            content: '';
+            position: absolute;
+            width: 15px;
+            height: 1px;
+            background: #fff;
+            transition: 0.2s;
+            transform: translateY(-5px);
+            box-shadow: 0 5px 0 #fff;
+        }
+        
+        &::after {
+            content: '';
+            position: absolute;
+            width: 15px;
+            height: 1px;
+            background: #fff;
+            transition: 0.2s;
+            transform: translateY(5px);
+        }
+        
+        ${({isActive}) =>  isActive && `
+            &::before {
+                transform: translateY(0px) rotate(45deg);
+                box-shadow: 0 0 0 #fff;
+            }
+            &::after {
+                transform: translateY(0px) rotate(-45deg);
+            }`
+        };
+    `,
+
+    Menu: styled.div`
+
+    `
+}
+
+
 const HambergerMenu : React.FC = () => {
-    return <div></div>
+    const [isActive, setIsActive] = useState(false);
+    const onToggle = () => {
+        setIsActive(!isActive);
+     };
+
+    return (
+        <H.Wrapper>
+            <H.ActiveBtn onClick={onToggle} isActive={isActive}>
+
+            </H.ActiveBtn>
+            <H.Menu>
+
+            </H.Menu>
+        </H.Wrapper>
+    )
 }
 
 export default HambergerMenu;
